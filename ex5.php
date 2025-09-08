a6b460cf6665fc5a5f8230e36a7f488342454225
Skyshar96 <skyshar96@gmail.com>
2025-09-08 12:25:55 +0200

    ajout fonction

---
+++ ex5.php
@@ -0,0 +1,40 @@
+<?php
+$tab1 = [8, 34, 2, 87, 37];
+
+function getMax($array) {
+    return max($array);
+}
+
+echo "Le plus grand nombre est : " . getMax($tab1);
+
+function getMin($array) {
+    return min($array);
+}
+
+echo "Le plus petit nombre est : " . getMin($tab1);
+
+function hasDuplicates($array) {
+    return count($array) !== count(array_unique($array));
+}
+
+// Exemple d'utilisation :
+if (hasDuplicates($tab1)) {
+    echo "Il y a des doublons dans le tableau.";
+} else {
+    echo "Il n'y a pas de doublons dans le tableau.";
+}
+
+function isSortedAscending($array) {
+    $sorted = $array;
+    sort($sorted);
+    return $array === $sorted;
+}
+
+// Exemple d'utilisation :
+if (isSortedAscending($tab1)) {
+    echo "Le tableau est en ordre croissant.";
+} else {
+    echo "Le tableau n'est pas en ordre croissant.";
+}
+
+?>
